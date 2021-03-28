f2f6af296206c73a36bbb6385afd24b9dd847aca
Alanampersand <adoylealan@gmail.com>
2021-03-28 15:07:04 +0100

    Chapter 3 Commit

---
+++ Chapter 3.py
@@ -0,0 +1,15 @@
+import pandas as pd
+import numpy as np
+
+temperatures=pd.read_csv("temperatures.csv")
+
+# Index temperatures by country & city
+temperatures_ind = temperatures.set_index(["country", "city"])
+
+# List of tuples: Brazil, Rio De Janeiro & Pakistan, Lahore
+rows_to_keep = [("Brazil", "Rio De Janeiro"), ("Pakistan", "Lahore")]
+
+# Subset for rows to keep
+print(temperatures_ind.loc[rows_to_keep])
+
+print("test")
